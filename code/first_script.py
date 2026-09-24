34677afcdc92e2dbeee5d8e6807857566f351983
dbs10000 <d.burnsidesmith@gmail.com>
2026-09-24 11:48:55 +0100

    Created my first feedparser script which works

---
+++ code/first_script.py
@@ -0,0 +1,68 @@
+# =========================================================
+# NEWS
+# ------------------------------
+# I created this python script to practice the feedparser module with python.
+# =========================================================
+
+
+
+
+# ------------------------------
+# MODULES
+# ------------------------------
+# I need the feedparser module for the script to work
+import feedparser
+
+
+# ------------------------------
+# URL
+# ------------------------------
+# BBC News Front Page
+url = "https://feeds.bbci.co.uk/news/rss.xml?edition=uk"
+
+
+
+# ------------------------------
+# PARSING THE RSS FEED
+# ------------------------------
+
+# Fetches and parses the RSS feed into a FeedParserDict object
+newsfeed = feedparser.parse(url)
+
+# Extracts the most recent article (first item in the feed)
+entry = newsfeed.entries[0]
+
+
+
+# ------------------------------
+# GETTING THE KEYS
+# ------------------------------
+# This code gives me the keys that exist:
+# print(entry.keys())
+
+# Output:
+# -------
+# 'title'
+# 'title_detail'
+# 'summary'
+# 'summary_detail'
+# 'links'
+# 'link'
+# 'id'
+# 'guidislink'
+# 'published'
+# 'published_parsed'
+# 'media_thumbnail'
+# 'href'
+
+# I can now use these keys to get the information that I want.
+
+
+
+# ------------------------------
+# GETTING STUFF
+# ------------------------------
+# prints the title
+print(f"Post Title: {entry.title}")
+# prints the summary
+print(f"Post Summary: {entry.summary}")
